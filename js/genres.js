59327a2c5823f05bf876228f96333a679e0b9336
santinolevy <slevy@udesa.edu.ar>
2022-11-22 19:07:50 -0300

    genres js

--- js/genres.js
+++ js/genres.js
@@ -1,122 +1,65 @@
  //Validacion del formulario//
-
- let formulario = document.querySelector(".buscador");
- let inputField = document.querySelector("#navigation");
  
-
+ let formulario = document.querySelector(".buscadorHeader");
+ let inputField = document.querySelector("#buscadorHeaderInput");
  formulario.addEventListener("submit", function (evento) {
    evento.preventDefault()
      if (inputField.value == "") {
-         alert("La busqueda no puede estar vacia!!")
+         alert("Ingrese texto")
      } else if (inputField.value.length <= 3) {
          alert("El termino a buscar debe tener al menos tres caracteres")
      } else {
          this.submit();
      }
- 
  });
-
-//Peliculas Populares//
-let urlPopularMovies = "https://api.themoviedb.org/3/movie/popular?api_key=4bcb2ca1395628db6221ba6939b8c9d7&language=en-US&page=1";
-
-fetch(urlPopularMovies)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    let info = data.results;
-
-    let pelisPopulares = document.querySelector(
-      ".contenedorArticulosPeliculas"
-    );
-    
-    let pelisPopularesLista = "";
-
-    for (i = 0; i < 5; i++) {
-      pelisPopularesLista += `
-                <article class="articulosIndex">
-                    <div class="contenedorImagen">
-                        <a href="detail-pelis.html?id=${info[5].id}">
-                            <img src= "https://image.tmdb.org/t/p/w342/${info[i].poster_path}" alt="">
-                        </a>
-                    </div>
-                    <h3>${info[i].title}</h3>
-                    <p>${info[i].release_date}</p>
-                </article>`;
-    }
-    pelisPopulares.innerHTML = pelisPopularesLista;
-  })
-  .catch(function (error) {
-    console.log("El error fue: " + error);
-  });
-
-//Series Populares//
-
-let urlPopularSeries ="https://api.themoviedb.org/3/tv/popular?api_key=4bcb2ca1395628db6221ba6939b8c9d7";
-
-fetch(urlPopularSeries)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    let info = data.results;
-
-    let seriesPopulares = document.querySelector(
-      ".contenedorArticulosSeriesPopulares"
-    );
-    let seriesPopularesLista = "";
-
-    for (i = 0; i < 5; i++) {
-      seriesPopularesLista += `<article class="articulosIndex">
-                <div class="contenedorImagen">
-                    <a href="detail series.html?id=${info[i].id}">
-                        <img src= "https://image.tmdb.org/t/p/w342/${info[i].poster_path}" alt="">
-                    </a>
-                </div>
-                <h3>${info[i].original_name}</h3>
-                <p>${info[i].first_air_date}</p>
-            </article>`;
-    }
-    seriesPopulares.innerHTML = seriesPopularesLista;
-  })
-  .catch(function (error) {
-    console.log("El error fue: " + error);
-  });
-
-//Peliculas Mas Vistos//
-
-let urlTopRatedMovies =
-  "https://api.themoviedb.org/3/movie/top_rated?api_key=4bcb2ca1395628db6221ba6939b8c9d7";
-
-fetch(urlTopRatedMovies)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    let info = data.results;
-
-    let pelisTop = document.querySelector(".contenedorArticulosVisto");
-    let pelisTopLista = "";
-
-    for (i = 0; i < 5; i++) {
-      pelisTopLista += `<article class="articulosIndex">
-                <div class="contenedorImagen">
-                    <a href="detail-movie.html?id=${info[i].id}">
-                        <img src= "https://image.tmdb.org/t/p/w342/${info[i].poster_path}" alt="">
-                    </a>
-                </div>
-                <h3>${info[i].title}</h3>
-                <p>${info[i].release_date}</p>
-            </article>`;
-    }
-    pelisTop.innerHTML = pelisTopLista;
-  })
-  .catch(function (error) {
-    console.log("El error fue: " + error);
-  });
+    let urlGenresMovie ="https://api.themoviedb.org/3/genre/movie/list?api_key=b3c5fcee75fac3fd848851441479d3b4&language=en-US";
+    fetch(urlGenresMovie)
+    .then(function (response) {
+        return response.json();
+    })
+  
+    .then(function (data) {
+        console.log(data.genres);
+        let genresMovie = document.querySelector(".contenedorGenres");
+        let genresLista = "";
+        let info = data.genres;
+        for (let i = 0; i < 6; i++) {
+        console.log(data.genres[i]);
+        genresLista += ` <article class="articulosGenres"> <a href="detail-genres.html?id=${info[i].id}&nombre=${data.genres[i].name}"> <h2> ${data.genres[i].name} </h2> </a></article>`;
+        }
+        genresMovie.innerHTML += genresLista;
+    })
+  
+    .catch(function (error) {
+        console.log(error);
+    });
+  
+    //series
+  
+    let urlGenresSeries =
+    "https://api.themoviedb.org/3/genre/tv/list?api_key=b3c5fcee75fac3fd848851441479d3b4";
+  
+    fetch(urlGenresSeries)
+    .then(function (response) {
+        return response.json();
+    })
+  
+    .then(function (data) {
+        console.log(data.genres);
+        let info = data.genres;
+        let genresSeries = document.querySelector(".contenedorGenresSeries");
+        let genresListaSeries = "";
+  
+        for (let i = 0; i < 6; i++) {
+        console.log(data.genres[i]);
+        genresListaSeries += ` <article class="articulosGenres"> <a href="detail-genres.html?id=${info[i].id}&nombre=${data.genres[i].name}"> <h2> ${data.genres[i].name} </h2> </a></article>`;
+        }
+        genresSeries.innerHTML += genresListaSeries;
+    })
+  
+    .catch(function (error) {
+        console.log(error);
+    });
+  
+ 
+ 
