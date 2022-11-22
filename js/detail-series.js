4b188ef914c853399f65441f327c57e4fea77737
Florencio Moneta <florenciomoneta@Florencios-MacBook-Air.local>
2022-11-22 20:50:07 -0300

    cambios

--- js/detail-series.js
+++ js/detail-series.js
@@ -1,57 +1,55 @@
-let formulario = document.querySelector(".buscador");
-    let inputField = document.querySelector("#navigation");
-
-    formulario.addEventListener("submit", function (evento) {
-        evento.preventDefault()
-     if (inputField.value == "") {
-         alert("La busqueda no puede estar vacia!!")
-     } else if (inputField.value.length <= 3) {
-         alert("El termino a buscar debe tener al menos tres caracteres")
-     } else {
-         this.submit();
-     }
+//Validacion del formulario//
+ 
+let formulario = document.querySelector(".buscadorHeader");
+let inputField = document.querySelector("#buscadorHeaderInput");
+formulario.addEventListener("submit", function (evento) {
+  evento.preventDefault()
+    if (inputField.value == "") {
+        alert("Ingrese texto")
+    } else if (inputField.value.length <= 3) {
+        alert("El termino a buscar debe tener al menos tres caracteres")
+    } else {
+        this.submit();
+    }
+});
+ 
+let queryStringSeries = location.search;
+let queryStringObject = new URLSearchParams(queryStringSeries);
+let id = queryStringObject.get("id");
+ 
+let url = `https://api.themoviedb.org/3/tv/${id}?api_key=b3c5fcee75fac3fd848851441479d3b4`;
+ 
+fetch(url)
+ .then(function (response) {
+   return response.json();
+ })
+ 
+ .then(function (data) {
+   console.log(data);
+ 
+   let titulo = document.querySelector(".tituloSerie");
+   let imagen = document.querySelector(".imagenSerie");
+   let subtitulo = document.querySelector(".subtitulosDetalles");
+   let rating = document.querySelector(".ratingSerie");
+   let fechaEstreno = document.querySelector(".fechaEstrenoSerie");
+   let descripcionSerie = document.querySelector(".descripcionSerie");
+ 
+   titulo.innerText = data.original_name;
+   imagen.src = `https://image.tmdb.org/t/p/w342/${data.poster_path}`;
+   subtitulo.innerText = data.original_name;
+   rating.innerText = `Rating:${data.vote_average}`;
+   fechaEstreno.innerText = `Fecha de estreno: ${data.first_air_date}`;
+   descripcionSerie.innerText = data.overview;
+ 
+   let generos = data.genres;
+   let generoDetalle = document.querySelector(".genero");
+   let generosLista = "";
+   for (let i = 0; i < generos.length; i++) {
+     generosLista += `<a class="anclajeGeneros" href="detail-genres.html?id=${generos[i].id}&nombre=${generos[i].name}">${generos[i].name}</a>. `;
+   }
+ 
+   generoDetalle.innerHTML = generosLista;
+ })
+ .catch(function (error) {
+   console.log("El error fue: " + error);
  });
- //capturamos el id de la pelicula que queremos mostrar
-let query = location.search
-let queryObjeto = new URLSearchParams(query)
-let id = queryObjeto.get("id")
-let apiKey = "b3c5fcee75fac3fd848851441479d3b4"
-
-let urlMovieDetail = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
-
-fetch(urlMovieDetail)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-
-    let titulo = document.querySelector(".tituloDetalleSeries")
-    titulo.innerText = data.title
-
-    let imagen = document.querySelector(".img")
-    imagen.src = `https://image.tmdb.org/t/p/w342/${data.poster_path}`
-
-    let rating = document.querySelector(".rating")
-    rating.innerText = data.vote_average
-
-    let fechaDeEstreno = document.querySelector(".fechaDeEstreno")
-    fechaDeEstreno.innerText = data.release_date
-
-    let sinopsis = document.querySelector(".sinopsis")
-    sinopsis.innerText = data.overview
-
-    
-//Generos de la pelicula
-
-let generos = data.genres
-let generosDetalle = document.querySelector(".generos")
-let generosLista = ""
-
-for (let i = 0; i < generos.length; i++) {
-        generosLista += `<a href="detail-genres.html?id${generos[i].id}&nombre${generos[i].name}">${generos[i].name}</a>`
-}
-generosDetalle.innerHTML = generosLista;
-
-})
