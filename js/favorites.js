b5c257fe9056dd00865ed43692834a9e90fa9b63
santinolevy <slevy@udesa.edu.ar>
2022-11-23 16:19:52 -0300

    validacion formulario

---
+++ js/favorites.js
@@ -0,0 +1,16 @@
+//Validacion del formulario//
+
+let formulario = document.querySelector(".buscadorHeader");
+let inputField = document.querySelector("#buscadorHeaderInput");
+
+formulario.addEventListener("submit", function (evento) {
+  evento.preventDefault()
+    if (inputField.value == "") {
+        alert("La busqueda no puede estar vacia!!")
+    } else if (inputField.value.length <= 3) {
+        alert("El termino a buscar debe tener al menos tres caracteres")
+    } else {
+        this.submit();
+    }
+
+});
